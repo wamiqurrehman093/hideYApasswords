e153efffede1c4aee70e6860ccc9d5ab415ec174
Syed1-Hassan <rummanali109@gmail.com>
2019-03-09 05:39:34 +0500

    actual Commit

---
+++ classes/decrypt.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace encryterConsole
+{
+    class decrypt
+    {
+        public string funcDecrypt(string s, Dictionary<char, char> x)
+        {
+            string retDecrypt = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                
+                if (x.ContainsKey(s[i]))
+                {
+
+                    char result = x.Keys.FirstOrDefault(t => x[t] == s[i]);
+                    retDecrypt = retDecrypt+result;
+
+                }
+
+            }
+
+            return retDecrypt;
+        }
+    }
+}
---
+++ classes/encrypt.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+
+namespace encryterConsole
+{
+    class encrypt
+    {
+        public string funcEncrypt(string s, Dictionary<char, char> x)
+        {
+            string retEncrypt="";
+            for (int i = 0; i < s.Length; i++)
+            {
+                
+                if (x.ContainsKey(s[i]))
+            {
+                retEncrypt = retEncrypt + x[s[i]];
+            }
+               
+            }
+           
+            return retEncrypt;
+        }
+
+        public void saveEncrypt(string email, string code, string filepath)
+        {
+            StreamWriter h = new StreamWriter(filepath, true);
+            h.WriteLine(email + " :  " + code);
+            h.Close();
+        }
+
+
+    }
+}
